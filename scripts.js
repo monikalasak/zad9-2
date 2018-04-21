5c6957085ba54fc1f3fc0732e6466e4993e72a0f
Monika Łasak <monika.k.lasak@gmail.com>
2018-04-21 13:29:33 +0200

    Change scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 //script.js
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-var allNames = Names.concat(femaleNames,maleNames);
+var allNames = allNames.concat(femaleNames,maleNames);
 
 console.log(allNames);
