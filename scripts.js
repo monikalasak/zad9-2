74aa998d964663c1e7e48432bd5538d9d95ab69f
Monika Łasak <monika.k.lasak@gmail.com>
2018-04-24 11:09:23 +0200

    Change scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,20 @@
 //script.js
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-var allNames = allNames.concat(femaleNames,maleNames);
+var allNames = femaleNames.concat( maleNames);
 
 console.log(allNames);
 
 var newName = 'Marian';
+var indexOfnewName = allNames.indexOf(newName);
+var x = allNames.push(newName);
 
-if () {}
+console.log('The search value is on ' + indexOfnewNames + ' position');
+console.log(indexOfnewName);
+
+if (allNames.indexOf(newName) === -1){
+	return x;
+} else {
+	return 'Imię jest już w tabeli'
+}
+
